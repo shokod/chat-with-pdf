db729dcfa211d802b3a2670caaa2682bee2bee10
shokod <delvinshoko@gmail.com>
2024-07-25 15:05:27 +0200

    Add! Features section

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,12 @@ import {
 } from "@radix-ui/react-icons";
 import {
   BrainCircuit,
+  BrainIcon,
   CloudUpload,
   EyeIcon,
+  Globe2Icon,
   MonitorSmartphoneIcon,
+  ZapIcon,
 } from "lucide-react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import "./globals.css";
@@ -20,20 +23,20 @@ const features = [
   {
     name: "Store your PDF Documents",
     description: "Securely store and access your PDFs anytime, anywhere.",
-    icon: GlobeIcon,
+    icon: Globe2Icon,
   },
   {
     name: "Blazing Fast Responses",
     description:
-      "Experience incredibly fast aswers t your quiries, ensuring you get the information you need instantly.",
-    icon: LightningBoltIcon,
+      "Experience incredibly fast aswers to your quiries, ensuring you get the information you need instantly.",
+    icon: ZapIcon,
   },
 
   {
     name: "Chat Memorisation",
     description:
       "Our intelligent chatbot remembers previous interactipons, provviding a seamless and personalized experience. Be assured it will stay incontext always!",
-    icon: BookmarkFilledIcon,
+    icon: BrainIcon,
   },
 
   {
@@ -59,15 +62,21 @@ const features = [
 
   {
     name: "Responsive Across Devices",
-    desription:
+    description:
       "Enjoy a seamless experience with optimized performance and layout on all your devices, from desktops to mobile phones.",
-    MonitorSmartphoneIcon,
+    icon: MonitorSmartphoneIcon,
+  },
+  {
+    name: "Effortless Document Management",
+    description:
+      "Manage your documents efficiently with our intuitive and user-friendly interface.",
+    icon: BookmarkFilledIcon,
   },
 ];
 
 export default function Home() {
   const [text] = useTypewriter({
-    words: ["Engage and Chat:"],
+    words: ["Engage and Convert:"],
     loop: false,
     delaySpeed: 2000,
   });
@@ -119,7 +128,26 @@ export default function Home() {
               height={1442}
               className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-slate-900/10"
             />
+            <div aria-hidden="true" className="relative">
+              <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white/95 pt-[7%]" />
+            </div>
           </div>
+        </div>
+
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 l:gap-y-16">
+            {features.map((feature) => (
+              <div className="relative pl-9">
+                <dt className="inline font-semibold text-slate-900">
+                  <feature.icon 
+                  aria-hidden="true"
+                  className="absolute left-1 top-1 h-5 w-5 text-fuchsia-600"/> 
+                </dt>
+
+                <dd>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
     </main>
