60884456434491b7d6b1bb1edcca577107405309
shokod <delvinshoko@gmail.com>
2024-07-25 09:33:21 +0200

    Add! React-Type Writer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider> 
       <html lang="en">
-        <body>{children}</body>
+        <body className="min-h-screen oh-screen overflow-hidden flex flex-col">{children}</body>
       </html>
     </ClerkProvider>
   );
